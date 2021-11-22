37c4f06534e02fd285518f1d1e260387733a3b59
ikebo <k_1043@126.com>
2021-11-22 14:51:07 +0800

    update docker file

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 mysqladmin -u root password $pwd
 mysql -uroot -p$pwd < /var/www/html/db.sql
 
+chmod -R 777 /var/www/html/usr/uploads
+
 nginx -c /etc/nginx/nginx.conf
