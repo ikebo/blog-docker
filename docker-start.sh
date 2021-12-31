c7bbee70523d755433764565c40407c8e9fee19a
ikebo <k_1043@126.com>
2021-12-31 16:38:51 +0800

    update start script

--- docker-start.sh
+++ docker-start.sh
@@ -1,1 +1,2 @@
-docker run --rm --name blog -v $(pwd)/start.sh:/root/start.sh -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled -v $(pwd)/ssl:/etc/nginx/ssl -v $(pwd)/src:/var/www/html  -it -p 80:80/tcp -p 443:443/tcp egbertke/blog
+docker run -d --name blog -v $(pwd)/start.sh:/root/start.sh -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled -v $(pwd)/ssl:/etc/nginx/ssl -v $(pwd)/src:/var/www/html  -it -p 80:80/tcp -p 443:443/tcp egbertke/blog
+nohup python3 -u flusher.py >flush.log 2>&1 &
